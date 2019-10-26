4c8604dd04965e475a3fcd35b8c1287e2b75f4c6
Andres Alba <bryanalba92@gmail.com>
2019-10-26 01:12:10 -0500

    Ruta para el login, buscar usuario y comprobar credenciales

--- controllers/user.js
+++ controllers/user.js
@@ -89,7 +89,69 @@ var controller = {
         }
 
 
+    },
+
+    login: function(req, res) {
+        // Recoger los parametros de la peticion 
+        var params = req.body;
+
+        // Validar los datos
+        var validate_email = !validator.isEmpty(params.email) && validator.isEmail(params.email);
+        var validate_password = !validator.isEmpty(params.password);
+
+        if (!validate_email || !validate_password) {
+            return res.status(200).send({
+                message: 'Los datos son incorrectos, envialos bien'
+            });
+        }
+
+        // Buscar los usuarios qie coincidan con el email
+        User.findOne({ email: params.email.toLowerCase() }, (err, user) => {
+
+            if (err) {
+                return res.status(500).send({
+                    message: 'Error al intentar identificarse'
+                });
+            }
+
+            if (!user) {
+                return res.status(404).send({
+                    message: 'El usuario no existe'
+                });
+            }
+
+            // si lo encuentra,
+            // Comprobar la contrasenia (coincidencia de email y password / bcrypt)
+            bcrypt.compare(params.password, user.password, (err, check) => {
+
+                // Si es correcto
+                if (check) {
+                    // Generar token de jwt y devolver (mas tarde)
+
+                    // Limpiar el objeto
+                    user.password = undefined;
+
+                    // Devolver los datos
+                    return res.status(200).send({
+                        status: 'success',
+                        user
+                    });
+                } else {
+                    return res.status(200).send({
+                        message: 'Las credenciales no son correctas'
+                    });
+                }
+
+
+
+            });
+        });
+
+
+
+
     }
+
 };
 
 module.exports = controller;
