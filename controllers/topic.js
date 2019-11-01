7740c333b827b1de85d781543800c51a17d7b22e
Andres Alba <bryanalba92@gmail.com>
2019-11-01 18:43:09 -0500

    Método para guardar nuevos temas

--- controllers/topic.js
+++ controllers/topic.js
@@ -5,6 +5,26 @@ var controller = {
         return res.status(200).send({
             message: 'Hola que tal'
         });
+    },
+
+    save: function(req, res) {
+
+        // Recoger parametros por post
+
+        // Validar datos
+
+        // Crear objeto guardar 
+
+        // Asignar valores
+
+        // Guardar el topic 
+
+        // Devolver una respuesta
+
+
+        return res.status(200).send({
+            message: 'soy metodo pra guardar topic'
+        });
     }
 }
 
